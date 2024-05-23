b493242573d15d071c6fcfbe3c459c66ee48f0aa
Loïc <55486724+nag763@users.noreply.github.com>
2024-05-23 20:00:25 +0200

    Done with index

--- src/components/index.rs
+++ src/components/index.rs
@@ -7,5 +7,19 @@ use crate::i18n::use_i18n;
 pub fn index() -> impl IntoView {
     let i18n = use_i18n();
 
-    view! { <h1>{t!(i18n, hello_world)}</h1> }
+    view! { 
+        <div class="h-full grid grid-cols-10 grid-rows-10 text-xl">
+            <div class="col-start-1 col-end-7 row-span-5 row-start-2 text-left space-y-2">
+                <p class="text-8xl">"LABEYE Loïc"</p>
+                <p class="pl-6 animate-fade-in-1">"💼  "{t!(i18n, job_title)}</p>
+                <p class="pl-6 animate-fade-in-2">"🎂  "{t!(i18n, age)}</p>
+                <p class="pl-6 animate-fade-in-3">"📍  "{t!(i18n, location)}</p>
+            </div>
+            <div class="col-start-6 col-end-10 text-left space-y-2 row-span-3 animate-fade-in-5">
+            <h1>{t!(i18n, introduction)}</h1>
+
+            </div>
+                <p class="flex flex-col row-span-1 col-span-full animate-pulse">{t!(i18n, scroll_down_to_continue)}</p>
+        </div>
+    }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,19 @@ module.exports = {
   content: ["./src/**/*.rs", "index.html"],
   theme: {
     extend: {
+      animation: {
+        'fade-in-1': 'fadeIn 1S ease-in-out',
+        'fade-in-2': 'fadeIn 2S ease-in-out',
+        'fade-in-3': 'fadeIn 3S ease-in-out',
+        'fade-in-5': 'fadeIn 3S ease-in-out',
+        'bounce-right': 'bounceRight 1s infinite',
+      },  
+      keyframes: {
+        fadeIn : {
+          '0%':  { opacity: 0 },
+          '100%': { opacity: 1 }
+        },
+      },
       gridTemplateRows: {
         '12': 'repeat(12, minmax(0, 1fr))',
       },
