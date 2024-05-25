69dc704e3f12e3948312ff6811d701017be826e0
Loïc <55486724+nag763@users.noreply.github.com>
2024-05-25 02:25:55 +0200

    Fixing issue as served under a particular path

--- src/components/projects.rs
+++ src/components/projects.rs
@@ -11,10 +11,10 @@ pub fn projects() -> impl IntoView {
         <p class="text-4xl">{t!(i18n, projects_title)}</p>
         <p class="text-xl">{t!(i18n, projects_subtitle)}</p>
         <div class="grid grid-cols-2 gap-2">
-            <ProjectCard title="tchatchers" img_src="/assets/tchatche.png" description={t!(i18n, projects.tchatche.description)()} card_href="https://tchatche.xyz/" />
-            <ProjectCard title="verbihr" img_src="/assets/verbihr.png" description={t!(i18n, projects.verbihr.description)()} card_href="https://nag763.github.io/verbihr/" />
-            <ProjectCard title="Snake game" img_src="/assets/snake.png" description={t!(i18n, projects.snake.description)()} card_href="https://nag763.github.io/texas-snake/" />
-            <ProjectCard title="Doteur" img_src="/assets/doteur.png" description={t!(i18n, projects.doteur.description)()} card_href="https://nag763.github.io/doteur/" />
+            <ProjectCard title="tchatchers" img_src="assets/tchatche.png" description={t!(i18n, projects.tchatche.description)()} card_href="https://tchatche.xyz/" />
+            <ProjectCard title="verbihr" img_src="assets/verbihr.png" description={t!(i18n, projects.verbihr.description)()} card_href="https://nag763.github.io/verbihr/" />
+            <ProjectCard title="Snake game" img_src="assets/snake.png" description={t!(i18n, projects.snake.description)()} card_href="https://nag763.github.io/texas-snake/" />
+            <ProjectCard title="Doteur" img_src="assets/doteur.png" description={t!(i18n, projects.doteur.description)()} card_href="https://nag763.github.io/doteur/" />
             </div>
         <p class="flex flex-col row-span-1 col-span-full animate-pulse">{t!(i18n, scroll_down_to_continue)}</p>
 
