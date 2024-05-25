037af0e19ce09b3b1cddba3209de2d594dc77e65
Loïc <55486724+nag763@users.noreply.github.com>
2024-05-25 22:17:57 +0200

    AdRefine white theme

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -17,7 +17,7 @@ pub(crate) mod common {
     ) -> impl IntoView {
         view! {
             <div >
-                <a href=card_href target={match card_href { "#" => "" , _ => "_blank"}} class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-600 h-full">
+                <a href=card_href target={match card_href { "#" => "" , _ => "_blank"}} class="flex flex-col items-center bg-white border border-gray-500 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-200 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-600 h-full">
                 <img class="object-cover w-full rounded-t-lg h-24 md:h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src=img_src alt="" />
                 <div class="flex flex-col justify-between p-4 leading-normal">
                     <h5 class="mb-2 text-lg md:text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
--- src/components/scholarship.rs
+++ src/components/scholarship.rs
@@ -12,10 +12,10 @@ pub fn chat_like_li(
 ) -> impl IntoView {
     view! {
         <li class="mb-10 ms-6">
-        <span class="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full md:-start-3 ring-2 ring-white dark:ring-gray-900 dark:bg-blue-900">
+        <span class="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full md:-start-3 ring-2 ring-black dark:ring-gray-900 dark:bg-blue-900">
             <img class="rounded-full shadow-lg" src=image_ref alt=image_alt/>
         </span>
-        <div class="items-center justify-between p-4 bg-white border border-gray-200 rounded-lg shadow-sm sm:flex dark:bg-gray-800 dark:border-gray-600">
+        <div class="items-center justify-between p-4 bg-white border border-gray-500 rounded-lg shadow-sm sm:flex dark:bg-gray-800 dark:border-gray-600">
             <time class="mb-1 text-xs font-normal text-gray-400 sm:order-last sm:mb-0">{time}</time>
             <div class="text-sm font-normal text-gray-500 dark:text-gray-300">{content}</div>
         </div>
