9e0253d93ec4dd6a26bffaf258406c5c1ac9bf10
Loïc <55486724+nag763@users.noreply.github.com>
2024-05-25 19:06:50 +0200

    Finish phone adapt

--- src/components/contact.rs
+++ src/components/contact.rs
@@ -8,10 +8,10 @@ pub fn contact() -> impl IntoView {
     let i18n = use_i18n();
     view! {
 
-        <div class="flex flex-col h-full justify-between py-4">
-        <p class="text-4xl">{t!(i18n, contact_title)}</p>
-        <p class="text-xl">{t!(i18n, contact_subtitle)}</p>
-        <div>
+        <div class="flex flex-col h-full justify-between py-4 overflow-scroll md:overflow-visible">
+        <p class="text-xl md:text-4xl">{t!(i18n, contact_title)}</p>
+        <p class="text-xs md:text-xl">{t!(i18n, contact_subtitle)}</p>
+        <div class="overflow-scroll md:overflow-visible">
             <form class="flex flex-col space-y-4" action="mailto:loic.labeye@pm.me" >
                 <div>
                     <label for="base-input" class="block text-left mb-2 text-sm font-medium text-gray-900 dark:text-white">{t!(i18n, contact.topic)}</label>
@@ -24,7 +24,7 @@ pub fn contact() -> impl IntoView {
                 <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">{t!(i18n, contact.send)}</button>
             </form>
         </div>
-        <p class="flex flex-col row-span-1 col-span-full ">{t!(i18n, last_section)}</p>
+        <p class="flex flex-col row-span-1 col-span-full text-xs md:text-base">{t!(i18n, last_section)}</p>
         </div>
 
     }
