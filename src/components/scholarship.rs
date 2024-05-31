4990577314c684cf7ef5d3d89c3b75dbaa64f13e
nag763 <loic.labeye@tutanota.de>
2024-05-31 19:17:04 +0200

    Fix animations

--- src/components/scholarship.rs
+++ src/components/scholarship.rs
@@ -16,7 +16,7 @@ pub fn scholarship() -> impl IntoView {
           <div class="timeline-middle">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" /></svg>
           </div>
-          <div class="timeline-start md:timeline-end text-start mb-10 animate-fade-left animate-once animate-ease-in">
+          <div class="timeline-start md:timeline-end text-start mb-10 animate-fade-left animate-duration-1000 animate-once animate-ease-in">
             <time class="font-mono italic">2021</time>
             <div class="text-lg font-black">{{t!(i18n, scholarship.graduated)}}</div>
           </div>
@@ -27,7 +27,7 @@ pub fn scholarship() -> impl IntoView {
           <div class="timeline-middle md:text-start">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" /></svg>
           </div>
-          <div class="timeline-start mb-10 text-start md:text-end animate-fade-right  animate-once animate-delay-[500ms] animate-ease-in">
+          <div class="timeline-start mb-10 text-start md:text-end animate-fade-right animate-duration-1000  animate-once animate-delay-[500ms] animate-ease-in">
           <time class="font-mono italic">2019-2020</time>
           <div class="text-lg font-black">{{t!(i18n, scholarship.belgium)}}</div>
           </div>
@@ -38,7 +38,7 @@ pub fn scholarship() -> impl IntoView {
           <div class="timeline-middle">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" /></svg>
           </div>
-          <div class="timeline-start md:timeline-end text-start mb-10 animate-fade-left animate-once animate-delay-[1000ms] animate-ease-in">
+          <div class="timeline-start md:timeline-end text-start mb-10 animate-fade-left animate-duration-1000 animate-once animate-delay-[1000ms] animate-ease-in">
           <time class="font-mono italic">2017</time>
           <div class="text-lg font-black">{{t!(i18n, scholarship.ireland)}}</div>
           </div>
@@ -49,7 +49,7 @@ pub fn scholarship() -> impl IntoView {
             <div class="timeline-middle md:text-start">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" /></svg>
             </div>
-            <div class="timeline-start mb-10 text-start md:text-end animate-fade-right animate-once animate-delay-[1500ms] animate-ease-in">
+            <div class="timeline-start mb-10 text-start md:text-end animate-fade-right animate-duration-1000 animate-once animate-delay-[1500ms] animate-ease-in">
             <time class="font-mono italic">2016</time>
             <div class="text-lg font-black">{{t!(i18n, scholarship.rouen)}}</div>
             </div>
