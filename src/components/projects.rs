72e0ade98e21573e1ce5c87c276eaa6955c72312
nag763 <loic.labeye@tutanota.de>
2024-06-01 00:54:18 +0200

    Fix render on phone

--- src/components/projects.rs
+++ src/components/projects.rs
@@ -13,14 +13,14 @@ fn card(
 ) -> impl IntoView {
     let i18n = use_i18n();
     view! {
-        <div class="card md:card-side max-h-full">
+        <div class="card md:card-side max-h-full items-center">
             <figure class="md:w-1/3"><img class="w-full h-auto" src=img_ref alt="Illustration"/></figure>
             <div class="card-body text-left md:w-2/3">
-            <h2 class="card-title">{title}</h2>
-            <p>{description}</p>
+            <h2 class="card-title mmd:text-sm">{title}</h2>
+            <p class="mmd:text-xs">{description}</p>
             <div class="card-actions justify-end">
             <a href=github_repo target="_blank">
-                <button class="btn btn-primary mmd:btn-sm"> <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" aria-label="Check out my Github !" viewBox="0 0 24 24" stroke="currentColor"><path d="M12 0C5.373 0 0 5.373 0 12c0 5.303 3.438 9.8 8.207 11.387.6.11.793-.258.793-.577 0-.285-.012-1.04-.018-2.04-3.22.702-3.89-1.54-3.89-1.54-.525-1.327-1.282-1.68-1.282-1.68-1.048-.715.08-.702.08-.702 1.16.082 1.773 1.2 1.773 1.2 1.033 1.77 2.713 1.258 3.37.96.105-.748.405-1.26.737-1.546-2.586-.294-5.297-1.293-5.297-5.74 0-1.27.45-2.312 1.2-3.126-.12-.296-.522-1.482.114-3.08 0 0 1.008-.312 3.3 1.2a11.115 11.115 0 012.947-.4c1.002.007 2.007.135 2.947.4 2.29-1.512 3.297-1.2 3.297-1.2.636 1.598.234 2.784.114 3.08.75.814 1.2 1.856 1.2 3.126 0 4.458-2.715 5.442-5.305 5.728.42.36.795 1.068.795 2.15 0 1.55-.015 2.8-.015 3.18 0 .318.21.694.8.576C20.568 21.797 24 16.3 24 12c0-6.627-5.373-12-12-12z"></path></svg> {t!(i18n, check_out_on_github)}</button>
+                <button class="btn btn-primary mmd:btn-xs"> <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 md:w-6 md:h-6" fill="none" aria-label="Check out my Github !" viewBox="0 0 24 24" stroke="currentColor"><path d="M12 0C5.373 0 0 5.373 0 12c0 5.303 3.438 9.8 8.207 11.387.6.11.793-.258.793-.577 0-.285-.012-1.04-.018-2.04-3.22.702-3.89-1.54-3.89-1.54-.525-1.327-1.282-1.68-1.282-1.68-1.048-.715.08-.702.08-.702 1.16.082 1.773 1.2 1.773 1.2 1.033 1.77 2.713 1.258 3.37.96.105-.748.405-1.26.737-1.546-2.586-.294-5.297-1.293-5.297-5.74 0-1.27.45-2.312 1.2-3.126-.12-.296-.522-1.482.114-3.08 0 0 1.008-.312 3.3 1.2a11.115 11.115 0 012.947-.4c1.002.007 2.007.135 2.947.4 2.29-1.512 3.297-1.2 3.297-1.2.636 1.598.234 2.784.114 3.08.75.814 1.2 1.856 1.2 3.126 0 4.458-2.715 5.442-5.305 5.728.42.36.795 1.068.795 2.15 0 1.55-.015 2.8-.015 3.18 0 .318.21.694.8.576C20.568 21.797 24 16.3 24 12c0-6.627-5.373-12-12-12z"></path></svg> {t!(i18n, check_out_on_github)}</button>
             </a>
             </div>
             </div>
@@ -42,8 +42,8 @@ pub fn projects() -> impl IntoView {
 
     let get_tabs = move || {
         view! {
-            <div role="tablist" class="tabs tabs-lifted px-2">
-            <a role="tab" class="tab" class:tab-active=move|| tab_index_val.get()==0 on:click=move|_| tab_index_set.set(0)>tchatchers</a>
+            <div role="tablist" class="tabs mmd:tabs-boxed mmd:tabs-xs md:tabs-lifted px-2">
+            <a role="tab" class="tab mmd:tab" class:tab-active=move|| tab_index_val.get()==0 on:click=move|_| tab_index_set.set(0)>tchatchers</a>
             <a role="tab" class="tab" class:tab-active=move|| tab_index_val.get()==1 on:click=move|_| tab_index_set.set(1)>verbihr</a>
             <a role="tab" class="tab" class:tab-active=move|| tab_index_val.get()==2 on:click=move|_| tab_index_set.set(2)>snake</a>
             <a role="tab" class="tab" class:tab-active=move|| tab_index_val.get()==3 on:click=move|_| tab_index_set.set(3)>doteur</a>
@@ -71,10 +71,12 @@ pub fn projects() -> impl IntoView {
         <Title text=t!(i18n, title.projects)/>
         <div class="flex flex-col items-center justify-between  py-4 overflow-y-auto 2xl:overflow-visible animate-fade animate-duration-100 animate-ease-in">
         <p class="text-xl 2xl:text-4xl row-span-1 justify-items-center ">{t!(i18n, projects_title)}</p>
-        <div class="mockup-phone md:hidden h-full">
-                <div class="h-full phone-1 flex flex-col">
-                    {get_tabs()}
+        <div class="mockup-phone w-full md:hidden h-full">
+        {get_tabs()}
+                <div class="phone-1 flex flex-col">
+
                     {move || get_card()}
+
             </div>
 
         </div>
