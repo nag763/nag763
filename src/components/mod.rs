6224e7600a706e75a7a93fa0bea6a815fffe7540
nag763 <loic.labeye@tutanota.de>
2024-06-03 19:16:04 +0200

    Fix rounded

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -17,7 +17,7 @@ pub(crate) mod common {
         view! {
             <div >
                 <div class="flex flex-col items-center bg-white border border-gray-500 rounded-lg shadow 2xl:flex-row 2xl:max-w-xl hover:bg-gray-200 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-600 h-full">
-                <img class="object-cover w-full rounded-t-lg h-24 2xl:h-96 2xl:h-auto 2xl:w-48 2xl:rounded-none 2xl:rounded-s-lg" src=img_src alt="" />
+                <img class="object-cover w-full rounded-t-lg h-24 2xl:h-96 2xl:h-auto 2xl:w-48 2xl:rounded-none" src=img_src alt="" />
                 <div class="flex flex-col justify-between p-4 leading-normal">
                     <h5 class="mb-2 text-lg 2xl:text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
                     <p class="mb-3 font-normal text-sm 2xl:text-base text-gray-700 dark:text-gray-400">{description}</p>
