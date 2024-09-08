cb74c51c2c51b633927df9999575898d259a0d2d
nag763 <loic.labeye@tutanota.de>
2024-09-08 21:17:44 +0200

    Fix overflow

--- src/components/post_scholarship.rs
+++ src/components/post_scholarship.rs
@@ -11,7 +11,7 @@ pub fn post_scholarship() -> impl IntoView {
         <Title text=t!(i18n, title.post_scholarship)/>
         <div class="flex flex-col h-full justify-between py-4 overflow-y-auto overflow-x-hidden items-center  2xl:items-stretch animate-fade animate-duration-100 animate-ease-in">
         <h1 class="row-span-1 text-xl 2xl:text-4xl max-xl:hidden">{t!(i18n, post_scholarship_title)}</h1>
-        <ul class="timeline timeline-snap-icon mmd:timeline-compact timeline-vertical row-span-4 overflow-y-auto max-h-full">
+        <ul class="timeline timeline-snap-icon mmd:timeline-compact timeline-vertical row-span-4 overflow-y-auto max-h-full overflow-x-hidden">
         <li >
           <div class="timeline-middle">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" /></svg>
