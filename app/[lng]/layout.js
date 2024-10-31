30854a804597736d2e8767b75da5cbae39556982
nag763 <loic.labeye@tutanota.de>
2024-10-31 21:41:16 +0100

    Update

--- app/[lng]/layout.js
+++ app/[lng]/layout.js
@@ -12,7 +12,7 @@ export default async function RootLayout({ children,
   return (
 
     <html className="h-full grid grid-cols-12">
-      <link rel="icon" href="../favicon.ico" sizes="any" />
+      <link rel="icon" href="favicon.ico" sizes="any" />
       <body className="h-full flex flex-col col-start-2 col-end-12">
         <Header />
         <main className="flex-1 grid grid-cols-12">
--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    webpack: (config) => {
-        config.resolve.alias = {
-            ...config.resolve.alias,
-            __dirname: false, // Ignorer __dirname dans Webpack
-            __filename: false,
-        }
-        return config;
-    }
+
 };
 
 export default nextConfig;
