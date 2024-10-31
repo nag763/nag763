5d2ba4995957a39f10d0465d92ddf5a8760e5360
nag763 <loic.labeye@tutanota.de>
2024-10-31 18:10:06 +0100

    Fixing the MW (I hope)

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+    webpack: (config) => {
+        config.resolve.alias = {
+            ...config.resolve.alias,
+            __dirname: false, // Ignorer __dirname dans Webpack
+            __filename: false,
+        }
+        return config;
+    }
 };
 
 export default nextConfig;
