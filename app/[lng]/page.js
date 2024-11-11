3b337c171cdfc42bc893d5a9413c8d9792133ec5
nag763 <loic.labeye@tutanota.de>
2024-11-11 17:54:41 +0100

    Add apropriate metadata given the language

--- app/[lng]/page.js
+++ app/[lng]/page.js
@@ -1,15 +1,21 @@
-
 import Contact from "./components/contact";
 import Experiences from "./components/experiences";
 import Hobbies from "./components/hobbies";
 import Intro from "./components/intro";
 import Projects from "./components/projects"
+import { getTranslations } from "../i18n";
 
 
-export const metadata = {
-    title: 'LABEYE Loïc',
-    description: 'Homepage of LABEYE Loïc CV\'s page'
-}
+export async function generateMetadata({ params : {lng} }) {
+    const {t} = await getTranslations(lng);
+    return {
+      title: t('meta.title'),
+      description: t('meta.description'),
+      keywords: "LABEYE Loïc, rust, javascript, Aubay, nextjs, python"
+      
+    }
+  }
+   
 
 
 export default async function Snapper({ params: { lng } }) {
