5970651404fd8ec8465e2d8bf304b3f99ac7e026
nag763 <loic.labeye@tutanota.de>
2025-02-19 18:21:50 +0100

    Add curr year to legal

--- app/[lng]/components/footer.js
+++ app/[lng]/components/footer.js
@@ -3,10 +3,11 @@ import { getTranslations } from "../../i18n";
 
 export default async function Footer({lng}){
     const { t } = await getTranslations(lng);
+    const currYear = new Date().getFullYear();
     return (
         <footer className="footer footer-center text-base-content rounded px-10 py-8">
             <aside>
-                <p>{t('legal')}</p>
+                <p>{t('legal', {currYear})}</p>
             </aside>
         </footer>
     );
