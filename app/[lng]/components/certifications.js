df06d7ef617795836de066c2bd5c0f29b9d1f034
nag763 <loic.labeye@tutanota.de>
2025-02-23 15:22:42 +0100

    Fix display on smaller screens

--- app/[lng]/components/certifications.js
+++ app/[lng]/components/certifications.js
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 async function CertificationCard({ certification, t }) {
     return (
-        <div className={`card shadow-lg bg-base-${certification.earned ? "200 hover:scale-105 hover:shadow-xl transition-all duration-300 ease-in-out"  : "100"}`}>
+        <div className={`card shadow-lg bg-base-${certification.earned ? "200 hover:scale-105 hover:shadow-xl transition-all duration-300 ease-in-out" : "100"}`}>
             <div className="card-body ">
                 <div className="flex flex-none justify-between space-x-4">
                     <div>
@@ -16,12 +16,12 @@ async function CertificationCard({ certification, t }) {
                         certification.earned ?
                             <div className="flex flex-col-reverse  md:flex-row  justify-center md:justify-end  gap-y-2 md:gap-y-0 md:gap-x-2  max-sm:text-sm">
                                 <Link href={t(`${certification.name}.page`)} target="_blank">
-                                    <button className="btn btn-secondary w-full md:w-auto max-md:btn-sm">
+                                    <button className="btn btn-secondary w-full max-md:h-full md:w-auto max-md:btn-sm">
                                         {t("view_ceritification_page")}
                                     </button>
                                 </Link>
                                 <Link href={t(`${certification.name}.check`)} target="_blank">
-                                    <button className="btn btn-primary w-full md:w-auto  max-md:btn-sm">
+                                    <button className="btn btn-primary w-full md:w-auto  max-md:h-full max-md:btn-sm">
                                         {t("check_myself")}
                                     </button>
                                 </Link>
--- app/[lng]/page.js
+++ app/[lng]/page.js
@@ -7,20 +7,21 @@ import { getTranslations } from "../i18n";
 import Certifications from "./components/certifications";
 
 
-export async function generateMetadata({ params  }) {
-    const { lng } = await params;
-    const {t} = await getTranslations(lng);
-    return {
-      title: t('meta.title'),
-      description: t('meta.description'),
-      keywords: "LABEYE Loïc, Rust, Javascript, TotalEnergies, Allianz Trade, NextJS, Python, Web, Software Engineer, Azure, Cloud, Microsoft",
-      robots: "index, follow",
-      viewport: "width=device-width, initial-scale=1.0",
-      charset: "UTF-8",
-    }
+export async function generateMetadata({ params }) {
+  const { lng } = await params;
+  const { t } = await getTranslations(lng);
+  return {
+    title: t('meta.title'),
+    description: t('meta.description'),
+    keywords: "LABEYE Loïc, Rust, Javascript, TotalEnergies, Allianz Trade, NextJS, Python, Web, Software Engineer, Azure, Cloud, Microsoft",
+    robots: "index, follow",
+    charset: "UTF-8",
   }
-   
+}
 
+export const viewport = {
+  viewport: "width=device-width, initial-scale=1.0",
+}
 
 export default async function Snapper({ params }) {
   const { lng } = await params;
