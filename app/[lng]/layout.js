8674733302a54282ac98467abc5608288c4d4785
nag763 <loic.labeye@tutanota.de>
2025-03-23 14:32:28 +0100

    Enable analytics and speed insights

--- app/[lng]/layout.js
+++ app/[lng]/layout.js
@@ -4,6 +4,8 @@ import "./components/header"
 import Header from "./components/header";
 import Footer from "./components/footer";
 import { Raleway } from 'next/font/google'
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const raleway = Raleway({ subsets: ['latin'] })
 
@@ -16,6 +18,8 @@ export default async function RootLayout({ children, params }) {
     <html className="h-full" lang={lng}>
       <link rel="icon" href="../favicon.ico" sizes="any" />
       <body className="flex flex-col w-full overflow-y-auto scroll-smooth h-dvh">
+        <Analytics/>
+        <SpeedInsights/>
         <Header className="flex-1  " />
         <main id="main" className={`snap-y max-h-full max-w-full overflow-y-auto space-y-12 xl:space-y-32 px-[10%] ${raleway.className}`}>
           {children}
