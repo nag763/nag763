6d5b2f6660c7159ffdb1871b10b76a95ddc4f41f
nag763 <loic.labeye@tutanota.de>
2025-04-13 10:10:25 +0200

    Minify html

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,8 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()],
-  },
+    compressHTML: true,
+    vite: {
+        plugins: [tailwindcss()],
+    },
 });
