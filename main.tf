56357668e16f9e6dd911c04065de016787c83e64
nag763 <loic.labeye@tutanota.de>
2025-05-07 16:35:11 +0200

    Put higher ttl as app changes not a lot

--- main.tf
+++ main.tf
@@ -160,9 +160,9 @@ resource "aws_cloudfront_distribution" "cdn" {
 
 
     viewer_protocol_policy = "redirect-to-https"
-    min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
+    min_ttl                = 3600
+    default_ttl            = 86400
+    max_ttl                = 1638400
     compress               = true
   }
 
