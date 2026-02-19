ccd17432ec827d52960a3b6b12f74b60d10a1d17
nag763 <loic.labeye@tutanota.de>
2026-02-19 18:24:11 +0100

    feat: update personal site

--- front/astro.config.mjs
+++ front/astro.config.mjs
@@ -1,14 +1,10 @@
-import { defineConfig } from "astro/config";
+// @ts-check
+import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
-import react from "@astrojs/react";
-
+// https://astro.build/config
 export default defineConfig({
-  compressHTML: true,
-
-  vite: {
-      plugins: [tailwindcss()],
-  },
-
-  integrations: [react()],
+    vite: {
+        plugins: [tailwindcss()]
+    }
 });
